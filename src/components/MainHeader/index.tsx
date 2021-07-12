886ca58e5dda38fac32ca59e68258e7743907edd
John <johnyei.business@gmail.com>
2021-07-12 17:18:18 -0300

    (feat) create header - username, random emojis with welcome message

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useMemo } from 'react';
+import emojis from '../../utils/emojis';
+import { Container, Profile, UserName, Welcome } from './styles';
 
 const MainHeader: React.FC = () => {
+    // useMemo - memoriza valores
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length); // emojis randomicos
+        return emojis[indice];
+    }, []);
     return (
         <>
             <Container>
-                <h1>MainHeader</h1>
+                <h1>Toggle</h1>
+
+                <Profile>
+                    <Welcome>Olá, {emoji}</Welcome>
+                    <UserName>Fiama de Capua</UserName>
+                </Profile>
             </Container>
         </>
     );
